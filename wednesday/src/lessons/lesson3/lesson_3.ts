7412945dc71cab5558e9f1df0b285df3d97801c9
Vladislav Olhovik <alkhovikvlad@gmail.com>
2020-11-07 18:19:52 +0100

    update lesson 11

--- wednesday/src/lessons/lesson3/lesson_3.ts
+++ wednesday/src/lessons/lesson3/lesson_3.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 console.log('lesson 3');
 
 // Event loop
@@ -13,7 +15,53 @@ console.log('lesson 3');
 
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
+const configOMB = {
+    baseURL: 'https://jsonplaceholder.typicode.com/posts',
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+};
+const axiosInstance2 = axios.create(configOMB);
 
+const get = async () => {
+    try {
+        const result = await axiosInstance2.get('/1')
+        console.log(result.data);
+
+        const list = await axiosInstance2.get('')
+        console.log('Get: ', list.data);
+
+        const post = await axiosInstance2.post('', {
+            body: JSON.stringify({
+                title: 'foo',
+                body: 'bar',
+                userId: 1,
+            }),
+        })
+        console.log('Post: ', post.data);
+
+        const put = await axiosInstance2.put('/1', {
+            body: JSON.stringify({
+                id: 1,
+                title: 'foo',
+                body: 'bar',
+                userId: 1,
+            }),
+        })
+        console.log('Put: ', put.data);
+
+        const patch = await axiosInstance2.patch('/1', {
+            body: JSON.stringify({
+                title: 'foo',
+              }),
+        })
+        console.log('Patch: ', patch.data);
+
+        const delet = await axiosInstance2.delete('/1')
+        console.log('Delete: ', delet.data);
+    } catch (e) { }
+}
+get()
 
 // just a plug
-export default ()=>{};
+export default () => { };
