2361742d2c08aba7662c4765d858e2454b770a69
Vladislav Olhovik <alkhovikvlad@gmail.com>
2020-11-30 23:42:30 +0100

    lesson 14 class

--- wednesday/src/lessons/lesson6/lesson6.ts
+++ wednesday/src/lessons/lesson6/lesson6.ts
@@ -1,6 +1,4 @@
-﻿import { log } from 'util';
-
-console.log('Lesson 6');
+﻿console.log('Lesson 6');
 
 // Class
 // https://learn.javascript.ru/classes
@@ -8,6 +6,125 @@ console.log('Lesson 6');
 // https://www.typescriptlang.org/docs/handbook/classes.html
 // https://www.youtube.com/watch?v=BASquaxab_w
 // https://www.youtube.com/watch?v=uLY9GXGMXaA
+
+// interface IpersonClass {
+//     name:string;
+//     age:number
+//     city: string
+// }
+// class Person implements IpersonClass {
+//     name:string;
+//     age:number
+//     city: string
+//     education: string 
+//     constructor(name:string, age:number, city:string, education: string){
+//         this.name = name
+//         this.age = age
+//         this.city = city
+//         this.education = education
+//     }
+//     sayHi(){
+//         console.log('hi i am Person');
+//     }
+// }
+
+// interface IspeekPerson extends IpersonClass {
+//     greeting: () => void
+// }
+// interface testPerson  {
+//     sayHi: () => void
+// }
+
+
+// class AngryPerson extends Person implements IspeekPerson, testPerson {
+//     constructor(name:string, age:number, city: string, education: string){
+//         super(name, age, city, education)
+//     }
+//     greeting(){
+//         console.log('What do you want');
+//     }
+//     sayHi(){
+//         console.log('hi');
+//         super.sayHi()
+//     }
+// }
+// class PolitePerson extends Person implements IspeekPerson {
+//     constructor(name:string, age:number, city: string, education: string){
+//         super(name, age, city, education)
+//     }
+//     greeting(){
+//         console.log('Hi');
+//     }
+// }
+
+// const testPerson = new Person ('Vlad', 26, 'Grodno', 'high')
+// console.log(testPerson);
+// const angryPerson = new AngryPerson ('Vlad', 26, 'Grodno', 'high')
+// console.log(angryPerson);
+// // angryPerson.greeting()
+// const politePerson = new PolitePerson ('Vlad', 26, 'Grodno', 'high')
+// console.log(politePerson);
+// // politePerson.greeting()
+// testPerson.sayHi()
+// angryPerson.sayHi()
+// politePerson.sayHi()
+
+
+//legacy code
+// class SuperTest {
+//     _name: string
+//     constructor(){
+//         this._name=''
+//     }
+//     get name (){
+//         return this._name
+//     }
+//     set name (name){
+//         this._name = name
+//     }
+// }
+
+//TS    
+
+// class SuperTest {
+//     private _name: string
+//     constructor(){
+//         this._name=''
+//     }
+//     get name (){
+//         return this._name
+//     }
+//     set name (name){
+//         this._name = name
+//     }
+// }
+// console.log(new SuperTest());
+// let pr = new SuperTest()
+// pr._name //Property '_name' is private and only accessible within class 'SuperTest'
+
+//static class
+
+// interface IMan {
+//     name: string
+//     sureName: string
+// }
+
+// class Test implements IMan {
+//     static greting = 'I am '
+//     name: string
+//     sureName: string
+//     constructor(name: string, sureName: string){
+//         this.name=name
+//         this.sureName=sureName
+//     }
+//     static getFullName(obj:IMan){
+//         return this.greting+obj.name+' '+obj.sureName
+//     }
+// }
+
+// let tesObj = new Test( 'test', 'testovich')
+// console.log(tesObj);
+// console.log(Test.getFullName(tesObj));
 
 
 // Task 01
@@ -31,11 +148,112 @@ interface IVehicle {
     startMoving: (maxSpeed?: number) => void;
     stopMoving: () => void;
 }
+class Vehicle implements IVehicle {
+    isStartEngine: boolean;
+    isMoving: boolean;
+    speed: number;
+    constructor () {
+        this.isStartEngine = false
+        this.isMoving = false
+        this.speed = 0
+    }
+    startEngine () {
+        if(this.isStartEngine) {
+            console.log('Are you sure? The engine is working now!');
+        } else {
+            this.isStartEngine = true
+            console.log('The engine is working now!');
+        }
+    }
+    stopEngine(){
+        if(!this.isStartEngine){
+            console.log('Hmm... How to stop not working engine?!');
+        } else if (this.isMoving) {
+            console.log('It is not a good idea to stop the engine');
+        } else {
+            this.isStartEngine = false
+            console.log('Engine was stop');
+        }
+    }
+    startMoving(maxSpeed = 150){
+        if(!this.isStartEngine){
+            console.log('Cannot move');
+        } else if (this.isMoving){
+            console.log('We are already moving!');
+        } else {
+            this.isMoving = true
+            let i = 1
+            while (this.speed<=maxSpeed){
+                setTimeout((speed)=>{
+                    if(speed > 120){
+                        console.log(`The speed is ${speed}! We are moving too fast!`);
+                    } else {
+                        console.log(`The speed is ${speed}!`);
+                    }
+                }, i*1000, this.speed)
+                this.speed+=15
+                i++
+            }
+            this.speed = maxSpeed
+            setTimeout(()=>console.log(`The speed is ${this.speed}!! we are going to die!!!`),i*1000)
+        }
+    }
+    stopMoving(){
+        if (this.speed === 0){
+            console.log('We are not moving, dude!!!');
+        } else {
+            let i = 1
+            while (this.speed>0){
+                setTimeout((speed)=>{
+                    if(speed < 40){
+                        console.log(`The speed is ${speed}! Huh we won't die!!`);
+                    } else {
+                        console.log(`The speed is ${speed}!`);
+                    }
+                }, i*1000, this.speed)
+                this.speed-=15
+                i++
+            }
+            setTimeout(()=>console.log(`The speed is ${this.speed}!! we arrived`),i*1000)
+            this.isMoving = false    
+        
+        }
+    }
+}
+
+const vehicle = new Vehicle()
+//@ts-ignore
+window.vehicle = vehicle
 
 // Task 02
 // на базе класса Vehicle реализовать класс Motorbike у которого максимальная скорость 200 и
 // есть свой метод burnOut который выводит в консоль 'Zoom zoom zoom zoom zoom...'
+class Motorbike extends Vehicle {
+    burnOut () {
+        if (!this.isStartEngine){
+            console.log('Start the engine bro');
+        } else {
+            console.log('Zoom Zoom Zoom Zoom Zoom');
+        }
+    }
+    startMoving(){
+        super.startMoving(200)
+    }
+}
 
+const bike = new Motorbike()
+bike.startEngine()
+bike.burnOut()
+bike.startMoving()
+
+class Car extends Vehicle {
+    startMoving(){
+        super.startMoving(180)
+    }
+}
+const car = new Car()
+car.startEngine()
+car.startMoving()
 // Task 03
 // на базе класса Vehicle реализовать класс Car у которого максимальная скорость 180 и
 // есть свой метод beep который выводит в консоль 'Beeeeeeeeeeeep...'
